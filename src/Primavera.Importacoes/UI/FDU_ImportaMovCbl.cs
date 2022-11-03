14ea701d4777d056eab890d7e9b77dc3dd8c4456
guimaraes.mahota <guimaraes.mahota@salc.dlapiperafrica.com>
2022-11-03 09:00:38 +0200

    importa mov cbl

---
+++ src/Primavera.Importacoes/UI/FDU_ImportaMovCbl.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Primavera.Extensibility.BusinessEntities;
+using Primavera.Extensibility.CustomForm;
+using Primavera.Importacoes.Services;
+
+namespace Primavera.Importacoes.UI
+{
+    public partial class FDU_ImportaMovCbl : CustomForm
+    {
+        public FDU_ImportaMovCbl()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string fileName = txtCaminhoExcel.Text;
+                int sheet = cbSheet.SelectedIndex;
+
+                dgDados.DataSource = motor.CarregaListaExcel(fileName, sheet, 2).DefaultView;
+            }
+            catch (Exception ex)
+            {
+                PSO.Dialogos.MostraAviso(
+                    "Ocorreu um erro ao carregar a informação do excell",
+                    StdPlatBS100.StdBSTipos.IconId.PRI_Critico,
+                    ex.Message
+                );
+            }
+        }
+    }
+}
